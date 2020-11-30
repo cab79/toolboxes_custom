33ba906a321b5b8dc563e17c6e8f33cd9b049cd7
cab79 <cb802@cam.ac.uk>
2020-11-30 17:36:48 +0000

    up

--- SCIn/Functions/SimpleSequence.m
+++ SCIn/Functions/SimpleSequence.m
@@ -6,7 +6,13 @@ h.Seq =struct;
 
 %% create final sequences/blocks
 if ~isfield(h.Seq,'signal')
-    h.Seq.signal=ones(1,h.Settings.ntrials);
-    h.Seq.condnum=ones(1,h.Settings.ntrials);
-    h.Seq.blocks=ones(1,h.Settings.ntrials);
+    if length(h.Settings.stim.dur) > 1
+        h.Seq.signal=1:h.Settings.ntrials;
+        h.Seq.condnum=1:h.Settings.ntrials;
+        h.Seq.blocks=ones(1,h.Settings.ntrials);
+    else
+        h.Seq.signal=ones(1,h.Settings.ntrials);
+        h.Seq.condnum=ones(1,h.Settings.ntrials);
+        h.Seq.blocks=ones(1,h.Settings.ntrials);
+    end
 end
